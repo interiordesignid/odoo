97203e0feb3b7421216d2009bea5aa9b03a090ee
interiordesignid <78422256+interiordesignid@users.noreply.github.com>
2021-02-23 10:31:14 +0700

    Add files via upload

---
+++ asteris/odoo/ast_id_project/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Interior Design Custom Project',
+    'category': 'Tools',
+    'summary': 'Interior Design Custom Project',
+    'description': """
+This module customizes Project module to suit Interior Design needs
+""",
+	"author" : "PT. Asteris Digital Lab",
+    "website" : "https://asteris.id",
+    'depends': ['base','sale','project'],
+    'data': [
+        'ast_id_project.xml',
+    ],
+    'application': True,
+}
